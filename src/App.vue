cbe4a676c152cb96befce2228e5f20bc355d077b
Paolo224 <picariellopaolo224@gmail.com>
2023-03-02 18:52:01 +0100

    chiamata axios + @use styles e bootstrap

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script>
+import { store } from './store.js';
+import axios from 'axios';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    AppMain,
+  },
+
+  data() {
+    return {
+      store,
+      urlAddress: 'http://127.0.0.1:8000/api/projects',
+    }
+  },
+
+  methods: {
+    getProjects() {
+      axios.get(this.urlAddress, {
+        params: {
+        }
+      })
+        .then((response) => {
+          this.store.projects = response.data.results;
+        })
+        .catch(function (error) {
+          console.warn(error);
+        });
+    }
+  },
+
+  created() {
+    this.getProjects();
+  },
+}
+
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use 'bootstrap/scss/bootstrap' as *;
+</style>
