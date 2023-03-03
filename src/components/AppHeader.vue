0d94fbfbe218b10767b8c805773bc98aabba3602
Paolo224 <picariellopaolo224@gmail.com>
2023-03-03 16:15:14 +0100

    Creazione del nuovo component con i router-link

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,62 @@
+<script>
+import { store } from '../store.js';
+export default {
+    name: 'AppHeader',
+
+    data() {
+        return {
+            store,
+        }
+    },
+
+    methods: {
+
+    },
+
+    created() {
+
+    },
+
+}
+</script>
+
+<template>
+    <header class="container">
+        <nav>
+            <ul>
+                <li>
+                    <router-link :to="{ name: 'home' }">
+                        Home
+                    </router-link>
+                </li>
+                <li>
+                    <router-link :to="{ name: 'about' }">
+                        About
+                    </router-link>
+                </li>
+                <li>
+                    <router-link :to="{ name: 'projects' }">
+                        Projects
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped lang="scss">
+nav {
+    width: 100%;
+    padding: 1rem;
+
+    ul {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+
+        li {
+            list-style-type: none;
+        }
+    }
+}
+</style>
