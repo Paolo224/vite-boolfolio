e3925f80f0b4f96edc33d566fef3a6bd3978963b
Paolo224 <picariellopaolo224@gmail.com>
2023-03-03 17:38:01 +0100

    creazione component AppProject + le props

---
+++ src/components/AppProject.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: 'AppProject',
+
+  props: {
+    'project': {
+      type: Object,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+
+    }
+  },
+};
+
+</script>
+
+<template>
+  <div class="col-4 d-flex align-items-stretch">
+    <div class="card mb-2">
+      <div class="card-body">
+        <h5 class="card-title">{{ project.Nome_progetto }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{ project.Descrizione_progetto }}</h6>
+        <p class="card-text">{{ project.Data_inizio_progetto }}</p>
+        <p class="card-text">{{ project.Data_fine_progetto }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
