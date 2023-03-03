dafdaafb2f71f9dcbdd1efa72ee278bc36320d49
Paolo224 <picariellopaolo224@gmail.com>
2023-03-03 17:38:44 +0100

    chiamata axios nella pages + v-for per i projects

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,13 +1,45 @@
 <script>
+import axios from 'axios';
+import AppProject from '../components/AppProject.vue';
 export default {
-    name: 'ProjectList'
+    name: 'ProjectList',
+    components: {
+        AppProject,
+    },
+    data() {
+        return {
+            projects: '',
+            urlAddress: 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+
+    methods: {
+        getProjects() {
+            axios.get(this.urlAddress, {
+                params: {
+                }
+            })
+                .then((response) => {
+                    this.projects = response.data.results;
+                })
+                .catch(function (error) {
+                    console.warn(error);
+                });
+        }
+    },
+
+    created() {
+        this.getProjects();
+    },
 }
 </script>
 
 <template>
-    <h1>
-        PROJECT
-    </h1>
+    <div class="container">
+        <div class="row">
+            <AppProject v-for="project in projects" :project="project" />
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss"></style>
